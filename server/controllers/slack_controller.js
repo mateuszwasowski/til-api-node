c6f87b6ac00b184e2de1c1416ef9e9e438e0960e
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-08-27 14:28:42 +0200

    update slack controller

--- server/controllers/slack_controller.js
+++ server/controllers/slack_controller.js
@@ -7,15 +7,33 @@ const pino = require("pino")();
 module.exports = {
   slacktil(req, res) {
     request("https://slack.com/api/users.list?token=" + process.env.SLACK_TOKEN, function(error, response, body) {
-      body = JSON.parse(body)
+      res.setHeader('Content-Type', 'application/json');
 
-      body.members.forEach(function (member) {
+      req.body.members.forEach(function (member) {
         if (member.name === req.body.user_name){
-          var user_email = member.email
+          Author.find({
+            where: {
+              email: member.email
+            }
+          })
+          .then(author => {
+            if (!author) {
+              return res.status(200).send(JSON.stringify({
+                 response_type: "ephemeral",
+                  text: "You first have to authorize google through the til web app to connect your account."
+              }));
+            }
+            Til.create({
+              description: req.body.description,
+              authorId: author.id,
+            })
+            .then({
+              res.status(200).send(JSON.stringify("Your Til has been submited!"));
+            })
+            .catch(error => res.status(400).send(error));
+          })
         }
       })
-      pino.info(user_email);
-
     });
   }
 }
