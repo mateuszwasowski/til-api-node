3547b5d413fc4490d6f9f07708fe2990999d58f7
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-08-27 15:22:21 +0200

    test

--- server/models/til.js
+++ server/models/til.js
@@ -20,14 +20,13 @@ module.exports = (sequelize, DataTypes) => {
   Til.afterCreate('postToChannel', (til, options) => {
     pino.info("HELLO!?");
     pino.info(til.description);
-    pino.info(til.author.email);
     request({
         url: "https://hooks.slack.com/services/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",
         method: "POST",
         headers: {
             "content-type": "application/json",
         },
-        body: JSON.stringify({ text: til.author.email + "posted:" + til.description })
+        body: JSON.stringify({ text: "posted:" + til.description })
     });
   });
   return Til;
