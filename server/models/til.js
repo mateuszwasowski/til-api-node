a1f5ae1b82cab03376d8a4d8f361d532ff1994c6
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-08-27 15:48:47 +0200

    fix message send slak

--- server/models/til.js
+++ server/models/til.js
@@ -1,6 +1,4 @@
 const request = require("request");
-const pino = require("pino")();
-const Author = require('../models').Author;
 
 module.exports = (sequelize, DataTypes) => {
   const Til = sequelize.define('Til', {
@@ -20,21 +18,15 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   Til.afterCreate('postToChannel', (til, options) => {
-    Author.findById(til.authorId)
-    .then(author => {
-      pino.info(author.email);
-    });
-    pino.info("FUN!");
-    til.getAuthor().then(author =>{
-      pino.info(author.email);
-    });
-    request({
-        url: "https://hooks.slack.com/services/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",
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({ text: til.getAuthor().email + " posted: \n" + til.description })
+    til.getAuthor().then(author => {
+      request({
+          url: "https://hooks.slack.com/services/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",
+          method: "POST",
+          headers: {
+              "content-type": "application/json",
+          },
+          body: JSON.stringify({ text: author.email + " posted: \n" + til.description })
+      });
     });
   });
   return Til;
