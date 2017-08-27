3b3fae102a52e13d80c609e6e1d674834aec62ae
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-08-27 09:38:07 +0200

    testing pino

--- server/services/google_oauth2_service.js
+++ server/services/google_oauth2_service.js
@@ -2,7 +2,11 @@ const pino = require('pino')()
 
 module.exports = function GoogleOuath2Service() {
   this.run = function () {
+    pino.info("WHAT")
     pino.info(process.env.CLIENT_ID)
+    pino.error(process.env.CLIENT_ID)
+    pino.info(process.env.CLIENT_ID)
+    pino.info("HELLO")
     return "hello"
   }
 };
