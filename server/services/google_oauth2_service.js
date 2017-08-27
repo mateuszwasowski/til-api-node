029e50a22fd4d0ceed5c738f043834f416ba25ef
Mateusz Wąsowski <mateusz.wasowski@netguru.pl>
2017-08-27 09:16:49 +0200

    env var testing

--- server/services/google_oauth2_service.js
+++ server/services/google_oauth2_service.js
@@ -2,5 +2,7 @@ module.exports = function GoogleOuath2Service() {
   this.run = function () {
     return "hello"
     console.log('hello');
+    const client_id = process.env.CLIENT_ID || "null";
+    console.log(client_id);
   }
 };
